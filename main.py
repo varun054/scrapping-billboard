d084c4525b7ef8aed38f4515051f783904fb328a
Varun Yadav <varun@makedoingeasier.com>
2025-01-05 09:40:17 +0530

    Spotify token and billboard scrapping project

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-CLIENT_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+CLIENT_KEY = "XXXXXXXX8b94b568c73fc3dbcf66914"
+CLIENT_SECRET = "XXXXXXXXX5d393674de7be9d2fae"
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -20,14 +20,31 @@ sp = spotipy.Spotify(
 user_id = sp.current_user()["id"]
 
 # Scrap top 100 song titles from billboard website.
-year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
-url = f"https://www.billboard.com/charts/hot-100/{year}"
+# year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:")
+# url = f"https://www.billboard.com/charts/hot-100/{year}"
 
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+# header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 
-response = requests.get(url=url, headers=header)
+# response = requests.get(url=url, headers=header)
 
-soup = BeautifulSoup(response.text, "html.parser")
-top_100_songs = soup.select(".lrv-u-width-100p > ul > li > h3")
-song_names = [song.getText().strip() for song in top_100_songs]
-print(song_names)
+# soup = BeautifulSoup(response.text, "html.parser")
+# top_100_songs = soup.select(".lrv-u-width-100p > ul > li > h3")
+# song_names = [song.getText().strip() for song in top_100_songs]
+# print(song_names)
+
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_names = ["Kitni hasrat hai", "Dil to pagal hai", "Dil to bachha hai"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+# Create spotify playlist.
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Varun Best Song", public=False)
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
